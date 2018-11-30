c356ba5cb624ba922f06ef5416f36ba62184abc9
Christoph Schuerz <christoph.schuerz@boku.ac.at>
2018-11-29 22:25:28 -0600

    updated weather example

--- ars_weather_test.R
+++ ars_weather_test.R
@@ -7,7 +7,7 @@ file_path <- "/home/christoph/Documents/ars_weather"
 
 ars_url <- "https://www.ars.usda.gov/ARSUserFiles/30980000/riesel/climate"
 
-weather_files <- 2008:2012%&%"hrly.txt"
+weather_files <- 2010:2012%&%"hrly.txt"
 
 tbl_header <- c("year",        # yyyy
                 "day",         # jdn
@@ -28,8 +28,29 @@ tbl_header <- c("year",        # yyyy
                 "t_sol_min"    # degC
                 )
 
-ars_clim <- map_dfr(weather_files, ~read_table(file = url(ars_url%//%.x), 
+ars_clim <- map_dfr(weather_files, ~read_table(file = url(ars_url%//%.x), guess_max =  10000,
                                                skip = 3, col_names = F)) %>% 
   set_names(tbl_header)
 
-save_
+saveRDS(ars_clim, "/home/christoph/Documents/projects/SWAT-R-Workshop/ars_clim.rds")
+
+y_jdn <- function(y, jdn) {
+  date <- ymd(y%//%"01"%//%"01")
+  yday(date) <- jdn
+  return(date)
+}
+
+y_jdn_h <- function(y, jdn, h) {
+  date <- ymd_h(y%//%"01"%//%"01"%&&%h)
+  yday(date) <- jdn
+  return(date)
+}
+
+ars_clim_1 <- ars_clim %>% 
+  filter(!is.na(year), year > 2000) %>% 
+  mutate(date = y_jdn(year, day)) %>% 
+  mutate(rh_ave = (rh_max + rh_min)/2) %>% 
+  group_by(year, day) %>% 
+  summarize(pr = sum(pr_tot), t_min = min(t_air_ave))
+  
+a <- read.table(file = url(ars_url%//%weather_files[1]), sep = "", skip = 3)
